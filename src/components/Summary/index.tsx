9632f15f0fec820f7e668f1e5318e45b705cf4d3
joaovitor <joaobuarqueestudos@gmail.com>
2021-09-21 22:38:42 -0300

    chapter-2-dtmoney

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import incomeImg from "../../assets/income.svg"
+import outcomeImg from "../../assets/outcome.svg"
+import totalImg from "../../assets/total.svg"
+
+import { Container } from "./styled";
+
+export function Summary() {
+    return(
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong>R$1000</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saidas</p>
+                    <img src={outcomeImg} alt="Saídas" />
+                </header>
+                <strong>- R$500</strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>R$500</strong>
+            </div>
+        </Container>
+    )
+} 
